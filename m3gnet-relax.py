81e820c4941ba79c13c1a607969b970fe2aac330
Matthew Evans <git@ml-evs.science>
2023-08-30 10:26:31 +0100

    Add some matador-related tweaks to my own copy

--- m3gnet-relax.py
+++ m3gnet-relax.py
@@ -16,22 +16,25 @@ Will relax the structures matching pattern "LiN2/run1/*-orig.cell" and save the 
 
 
 import os
-os.environ["CUDA_VISIBLE_DEVICES"]="-1"    
-os.environ["OMP_NUM_THREADS"]="1"    
-os.environ["TF_NUM_INTRAOP_THREADS"]="1"    
-os.environ["TF_NUM_INTEROP_THREADS"]="1"    
 
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["TF_NUM_INTRAOP_THREADS"] = "1"
+os.environ["TF_NUM_INTEROP_THREADS"] = "1"
 
-# In[2]:
-import tensorflow as tf
 
 #
 import warnings
 from pathlib import Path
-from ase.io import read
-from m3gnet.models import Relaxer
-from disp.analysis.airssutils import RESFile
+
 import ase
+# In[2]:
+import tensorflow as tf
+from ase.io import read
+from disp.analysis.airssutils import RESFile
+from m3gnet.models import Relaxer
+from matador.crystal import Crystal
+from matador.scrapers import cell2dict
 from pymatgen.io.ase import AseAtomsAdaptor
 
 for category in (UserWarning, DeprecationWarning):
@@ -40,27 +43,26 @@ for category in (UserWarning, DeprecationWarning):
 from tqdm import tqdm
 
 
-
-
 def relax_one(cell, verbose=True, relaxer=None):
     """
     Relax one single structure
-    
+
     Returns relaxed `ase.Atoms` object and the final energy per atom
     """
-    
+
     if isinstance(cell, ase.Atoms):
         cell = AseAtomsAdaptor.get_structure(cell)
-   
+
     if relaxer is None:
         relaxer = Relaxer()
+
     relax_results = relaxer.relax(cell, verbose=verbose)
-    final_structure = relax_results['final_structure']
-    final_energy = float(relax_results['trajectory'].energies[-1])
+    final_structure = relax_results["final_structure"]
+    final_energy = float(relax_results["trajectory"].energies[-1])
     return final_structure, final_energy
 
 
-def process_one(cell, outdir='m3gnet-relaxed', relaxer=None):
+def process_one(cell, outdir="m3gnet-relaxed", relaxer=None):
     """Process a single atoms object and save the results as a SHELX file"""
 
     if relaxer is None:
@@ -83,13 +85,11 @@ if __name__ == "__main__":
     input_names = [Path(x) for x in sys.argv[2:]]
     outdir = sys.argv[1]
 
-    cells = list(map(read, input_names))
+    cells, warnings = cell2dict([str(d) for d in input_names])
+    cells = [Crystal(cell).ase_atoms for cell in cells]
     # Store the name of the files
     for atoms, name in zip(cells, input_names):
-        atoms.info['fname'] = name.stem
+        atoms.info["fname"] = name.stem
     # Create the Relaxer object
     relaxer = Relaxer()
     [process_one(x, outdir, relaxer) for x in tqdm(cells)]
-
-
-
